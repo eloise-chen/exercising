235a39333620ce1b876855d9da15e0b18452dd54
chene <eloise.y.chen@gmail.com>
2015-10-26 14:12:52 +0800

    Uses two different methods to send an email when items are added or updated to a list. Elements.xml is commented out and currently trying to attach the event receiver to the feature programmatically instead.

---
+++ Ex1_EventReceiver/Features/EmailItemAdded/EmailItemAdded.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9fd67099-c1a5-468e-a298-12d6f9873e4a" featureId="9fd67099-c1a5-468e-a298-12d6f9873e4a" imageUrl="" receiverAssembly="$SharePoint.Project.AssemblyFullName$" receiverClass="$SharePoint.Type.3a12309e-4727-4554-b4e3-27906d194b69.FullName$" solutionId="00000000-0000-0000-0000-000000000000" title="Ex1_EventReceiver Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="49407fe6-e3ca-43b6-af78-a95837b3a872" />
+  </projectItems>
+</feature>
---
+++ Ex1_EventReceiver/ListItemNotification/ListItemNotification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.Net.Mail;
+
+
+
+// For event receivers, don't use using to get current context/web because it will dispose of the object at the end and that may cause "Page cannot be displayed" errors or something.
+// Exception is when you want to access an object not in the current context. Other types of things can use using, like timer jobs
+
+// Next: Programmatically add the event receiver using a feature receiver instead of the xml way
+// Also: expand on the information in the email body using properties
+
+namespace Ex1_EventReceiver.ListItemNotification
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class ListItemNotification : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was added.
+        /// This function uses the SPUtility Send Email
+        /// Pro: uses the SharePoint farm's SMTP settings so the developer doesn't need to know them
+        /// Con: character limitation of 2048 per line, which can strip out the content of the email
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+
+                try
+                {
+                    SPUtility.SendEmail(properties.Web, false, false, "Eloise.Chen@smsmt.com", "EventReceiver - Add", "An item has been added to the Existing list.");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+        }
+
+        /// <summary>
+        /// An item was updated.
+        /// This function uses the System.Net.Mail.MailMessage
+        /// Pro: More granular control of the SMTP details and message, no character limitation of the message 
+        /// Con: Requires knowledge of the SMTP details
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+          
+                try
+                {
+                    MailMessage message = new MailMessage("eloise.chen@smstest.com", "eloise.chen@smsmt.com", "EventReceiver - Update", "An item has been updated on the Existing list.");
+                    SmtpClient client = new SmtpClient("d-int-sp02", 25);
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+        }
+    }
+}
